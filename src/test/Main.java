49149a1ad3e3a3766ba48b2b17dd3fe09552be2f
Vitor Rubem Lopes de Lima <vitor.lima@inpi.gov.br>
2019-10-03 17:17:28 -0300

    Primeiro Commit

---
+++ src/test/Main.java
@@ -0,0 +1,40 @@
+package test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Calendar before = Calendar.getInstance();
+
+		HttpURLConnection connection;
+		try {
+			connection = (HttpURLConnection)new URL( "http://integracao.inpi.gov.br/patentews/dados/BR1120170041014" ).openConnection();
+			connection.getResponseCode();
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		Calendar end = Calendar.getInstance();
+
+		//read the content
+
+		Calendar endContent = Calendar.getInstance();
+
+		System.out.println( end.getTimeInMillis() - before.getTimeInMillis());
+		System.out.println( endContent.getTimeInMillis() - end.getTimeInMillis());
+		
+	}
+
+}
